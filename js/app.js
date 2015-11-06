8e819a97108da08b8a7c5f3a3691258508b44254
chavraqul <raquel.chavez@hpe.com>
2015-11-05 21:35:48 -0600

    adding map your meetup groups app

---
+++ js/app.js
@@ -0,0 +1,88 @@
+$(document).ready(function(){
+  	/*--- Hide google maps modal box ---*/
+  	$("a.close").click(function(){
+  		$(".overlay").fadeOut(1000);
+  	});
+  	$(".search-criteria").submit( function(event){
+  		/*--- Display google maps modal box ---*/
+  		event.preventDefault();
+    	$(".overlay").fadeIn(1000);
+		
+		// get the value of user inputs
+		var place = $(this).find("input[name='city-or-zipcode']").val();
+		var meetups = $(this).find("select[name='meetups']").val();
+		var distance = $(this).find("select[name='distance']").val();
+
+		// call meetups api
+		getMeetups(place, meetups, distance);
+		setTimeout(function(){alert("Hello")},5000);
+	});
+});
+/*
+ * Meetups api call
+ * return findInMap
+ */
+function getMeetups (place, meetups, distance) {
+	// request parameters to pass to Meetup's API
+	/*
+	var request = {//zip: place, //string
+		           location: place, //string
+				   //radius: distance, //integer
+				   //category: meetups, //integer
+				   //sign: true,
+				   //photo-host: public,
+				   page:20,
+				   key: meetupsKey
+				   };
+	var promise = $.ajax({
+		url: "https://api.meetup.com/find/groups",
+		data: request,
+		contentType: "application/json",
+		type: "GET"
+		})
+	.done(function(result){
+		console.log(result);
+		$.each(result, function(i, result) {
+			//get Latitude and longitude to in map
+			var lat = result[i].lat;
+			var lng = result[i].lon;
+			// call google maps api
+			getGoogleMaps(lat, lng);
+		});
+	})
+	.fail(function(jqXHR, error, errorThrown){
+		//var errorElem = showError(error);
+		//$('#modal').append(errorElem);
+		console.log($.get("https://api.meetup.com/find/groups?location=Austin&page=20&key=87a2148299157a5f12667c280696b"));
+	});
+*/
+$.get('http://api.meetup.com/find/groups?location=Austin&page=20&key=87a2148299157a5f12667c280696b', function(data){
+	console.log(data);
+});
+
+};
+/*
+ * Google Maps api call
+ * @param findInMap
+ */
+function getGoogleMaps (lat, lng) {
+	// request parameters to pass to Google Map's API
+	var request = {key: googleMapskey,
+				   latlng:lat,lon
+				   };
+	var promise = $.ajax({
+		url: "https://maps.googleapis.com/maps/api/geocode",
+		data: request,
+		dataType: "json",
+		type: "GET"
+		});
+
+	promise.done(function(result){
+		console.log(result);
+		$('#modal').html(result);
+	})
+	promise.fail(function(jqXHR, error, errorThrown){
+		var errorElem = showError(error);
+		$('#modal').append(errorElem);
+	});
+};
---
+++ js/auth.js
@@ -0,0 +1,2 @@
+var googleMapskey = "AIzaSyDoFnjotXjbMjCVoaooxmZ4H2ekYsaC3b8";
+var meetupsKey = "87a2148299157a5f12667c280696b"
